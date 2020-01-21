c177678b375d54260d16a21aefb93f1d7562dea4
zhanyeye <zhanyeye@qq.com>
2020-01-21 23:36:49 +0800

    提交审核页面
    1. 修改element-ui 默认size
    2. 新建一个 test view 和 路由用于页面设计
    3. 修改 setting.js 关闭 right-panel
    4. 设计提交审核结果的drawer UI

--- src/permission.js
+++ src/permission.js
@@ -9,13 +9,12 @@ NProgress.configure({ showSpinner: false }); // NProgress Configuration
 const whiteList = ["/login"]; // no redirect whitelist
 
 router.beforeEach(async (to, from, next) => {
-  NProgress.start(); // 进度条开始
-
+  NProgress.start();
   if (sessionStorage.getItem("token")) {
     // 判断用户是否已登陆
     if (to.path === "/login") {
       next({ path: "/" }); // 如果已登录，重定向到'/'
-      NProgress.done(); // 进度条完成
+      NProgress.done();
     } else {
       const hasGetUserInfo = store.getters.name;
       if (hasGetUserInfo) {
@@ -23,31 +22,26 @@ router.beforeEach(async (to, from, next) => {
       } else {
         try {
           await store.dispatch("user/_getInfo"); // 获取用户信息
-
           // 基于角色生成可以获得的路由
           const accessRoutes = await store.dispatch(
             "permission/_generateRoutes",
             [sessionStorage.getItem("role")]
           );
-
-          // dynamically add accessible routes
+          // 动态添加可访问路由表
           router.addRoutes(accessRoutes);
-
-          // hack method to ensure that addRoutes is complete
-          // set the replace: true, so the navigation will not leave a history record
+          // hack方法 确保addRoutes已完成 ,set the replace: true so the navigation will not leave a history record
           next({ ...to, replace: true });
         } catch (error) {
           // remove token and go to login page to re-login
           //await store.dispatch("user/resetToken");
           Message.error(error || "Has Error");
           next(`/login?redirect=${to.path}`);
-          // NProgress.done();
+          NProgress.done();
         }
       }
     }
   } else {
     /* has no token*/
-
     if (whiteList.indexOf(to.path) !== -1) {
       // in the free login whitelist, go directly
       next();
--- src/settings.js
+++ src/settings.js
@@ -5,7 +5,7 @@ module.exports = {
    * @type {boolean} true | false
    * @description Whether show the settings right-panel
    */
-  showSettings: true,
+  showSettings: false,
 
   /**
    * @type {boolean} true | false

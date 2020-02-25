c65fc9be6a41888ed8a63ba5c20eb7553c444073
zhanyeye <zhanyeye@qq.com>
2020-02-25 18:00:16 +0800

    test kanban

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -1,50 +1,69 @@
 <template>
-  <div id="parent">
-    <div id="top">WebSocket Test</div>
-    <div id="bottom">
-      I need scroll here when this content overflow the parent.
+  <div class="app-container">
+    <div class="test">
+      <draggable v-model="list" class="test" :options="{ group: 'card' }">
+        <div v-for="(item, index) in list" :key="index" class="card" shadow="never">
+          从不显示{{ item }}
+
+          <draggable v-model="item.arr" :options="{ group: 'task' }">
+            <el-card v-for="task in item.arr" :key="task.index" class="task">
+              {{ task }}
+            </el-card>
+          </draggable>
+        </div>
+      </draggable>
     </div>
-    <img width="100" :src="imgUrl" />
-
-    <el-tooltip class="item" effect="dark" placement="top-start">
-      <div slot="content">
-        <span style="padding:5px;">asdf</span>
-        <span style="padding:5px;">asdfa</span>
-        <span style="padding:5px;"> asdf</span>
-        <span style="padding:5px;">asdfad</span>
-      </div>
-
-      <div class="tt">
-        <span style="padding:5px;">asdf</span>
-        <span style="padding:5px;">asdfa</span>
-        <span style="padding:5px;"> asdf</span>
-        <span style="padding:5px;">asdfad</span>
-      </div>
-    </el-tooltip>
   </div>
 </template>
 <script>
-import { getavatar } from "@/utils/initavatar";
-
+import draggable from "vuedraggable";
 export default {
   data() {
     return {
-      imgUrl: ""
+      list: [
+        {
+          arr: [34, 5]
+        },
+        {
+          arr: [45, 667, 56]
+        },
+        {
+          arr: [34, 56]
+        },
+        {
+          arr: [1, 2, 3, 4]
+        }
+      ]
+      // list: message.map((name, index) => {
+      //   return { name, order: index + 1, fixed: false };
+      // }),
     };
   },
-  created() {
-    this.imgUrl = getavatar("刘博涵");
+  components: {
+    draggable
   },
+  created() {},
   methods: {}
 };
 </script>
 
 <style scoped>
-.tt {
-  width: 100px;
-  justify-content: flex-start;
-  overflow: hidden; /*超出部分隐藏*/
-  white-space: nowrap;
-  text-overflow: ellipsis;
+.app-container {
+  background-color: #f5f5f5;
+  height: 92.8vh;
+  border-radius: 0;
+}
+.test {
+  display: flex;
+  overflow-x: auto;
+}
+.card {
+  margin: 5px;
+  flex-shrink: 0;
+  width: 45vh;
+  height: 85.5vh;
+}
+.task {
+  width: 100%;
 }
 </style>

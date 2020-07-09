6b89d18e48451a8e3ae618c3c01d52bcc5dfd407
zhanyeye <zhanyeye@qq.com>
2020-07-09 22:47:34 +0800

    Update index.vue

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -23,6 +23,16 @@ export default {
     if (!this.roles.includes("normal")) {
       this.currentRole = "auditorDashboard";
     }
+  },
+  mounted() {
+    let windowWidth = document.body.clientWidth;
+    if (windowWidth > 700 && windowWidth < 890) {
+      this.$message({
+        showClose: true,
+        message: "建议点击右上角的'独立窗口', 调整宽度，以提升使用体验",
+        offset: 32
+      });
+    }
   }
 };
 </script>

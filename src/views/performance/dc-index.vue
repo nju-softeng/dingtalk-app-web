162fb16b6c16e633600eed448a84839ba841baaa
zhanyeye <zhanyeye@qq.com>
2020-11-06 20:54:39 +0800

    更新绩效报表排序

--- src/views/performance/dc-index.vue
+++ src/views/performance/dc-index.vue
@@ -2,12 +2,16 @@
   <div class="app-container">
     <div class="box">
       <div>
-        <el-date-picker size="mini" v-model="date" :clearable="false" value-format="yyyy-MM-dd" @change="filtrate" type="month" style="width:120px" placeholder="选择月"> </el-date-picker>
+        <el-date-picker v-model="date" size="mini" :clearable="false" value-format="yyyy-MM-dd" type="month" style="width:120px" placeholder="选择月" @change="filtrate" />
         <el-button-group style="margin-left:5px">
-          <el-button @click="prev" icon="el-icon-arrow-left">上一月</el-button>
-          <el-button @click="next">下一月<i class="el-icon-arrow-right el-icon--right"></i></el-button>
+          <el-button icon="el-icon-arrow-left" @click="prev">上一月</el-button>
+          <el-button @click="next">下一月<i class="el-icon-arrow-right el-icon--right" /></el-button>
         </el-button-group>
-        <el-button :loading="downloadLoading" style="margin:0 0 5px 5px;"  icon="el-icon-document" @click="handleDownload">
+        <el-button-group style="margin-left:5px">
+          <el-button icon="el-icon-bottom-right" @click="desc" />
+          <el-button icon="el-icon-top-right" @click="asc" />
+        </el-button-group>
+        <el-button :loading="downloadLoading" style="margin:0 0 5px 5px;" icon="el-icon-document" @click="handleDownload">
           Export Excel
         </el-button>
       </div>
@@ -22,50 +26,49 @@
         style="margin-top:10px;"
         height="77.5vh"
         :header-cell-style="{ background: '#eef1f6' }"
-        :default-sort ="{prop:'salary',order:'descending'}"
       >
         <el-table-column fixed label="学号" align="center" width="90">
           <template slot-scope="{ row }">
             {{ row.stu_num || '未设置' }}
           </template>
         </el-table-column>
-        <el-table-column fixed prop="name"  label="姓名" align="center"> </el-table-column>
-        <el-table-column label="助研金" align="center" width="82" prop="salary" sortable>
+        <el-table-column fixed prop="name" label="姓名" align="center" />
+        <el-table-column label="助研金" align="center" width="82" prop="salary">
           <template slot-scope="{ row }">
             {{ row.salary || '-' }}
           </template>
         </el-table-column>
-        <el-table-column label="第1周DC" align="center" width="92" prop="week1" sortable>
+        <el-table-column label="第1周DC" align="center" width="92" prop="week1">
           <template slot-scope="{ row }">
             {{ row.week1 || '-' }}
           </template>
         </el-table-column>
-        <el-table-column label="第2周DC" align="center" width="92" prop="week2" sortable>
+        <el-table-column label="第2周DC" align="center" width="92" prop="week2">
           <template slot-scope="{ row }">
             {{ row.week2 || '-' }}
           </template>
         </el-table-column>
-        <el-table-column label="第3周DC" align="center" width="92" prop="week2" sortable>
+        <el-table-column label="第3周DC" align="center" width="92" prop="week2">
           <template slot-scope="{ row }">
             {{ row.week3 || '-' }}
           </template>
         </el-table-column>
-        <el-table-column label="第4周DC" align="center" width="92" prop="week3" sortable>
+        <el-table-column label="第4周DC" align="center" width="92" prop="week3">
           <template slot-scope="{ row }">
             {{ row.week4 || '-' }}
           </template>
         </el-table-column>
-        <el-table-column label="第5周DC" align="center" width="92" prop="week4" sortable>
+        <el-table-column label="第5周DC" align="center" width="92" prop="week4">
           <template slot-scope="{ row }">
             {{ row.week5 || '-' }}
           </template>
         </el-table-column>
-        <el-table-column label="总DC" align="center" width="80" prop="week5" sortable>
+        <el-table-column label="总DC" align="center" width="80" prop="week5">
           <template slot-scope="{ row }">
             {{ row.total || '-' }}
           </template>
         </el-table-column>
-        <el-table-column label="当前AC" align="center" width="85" prop="ac" sortable>
+        <el-table-column label="当前AC" align="center" width="85" prop="ac">
           <template slot-scope="{ row }">
             {{ row.ac || '-' }}
           </template>
@@ -75,22 +78,22 @@
             <template v-if="!row.edit">
               {{ row.topup || 0 }}
 
-              <el-button type="text" v-if="roles.includes('auditor') || roles.includes('admin')" icon="el-icon-edit" @click="row.edit = true" style="margin-left:16px"> </el-button>
+              <el-button v-if="roles.includes('auditor') || roles.includes('admin')" type="text" icon="el-icon-edit" style="margin-left:16px" @click="row.edit = true" />
             </template>
             <template v-else>
-              <el-input v-model="row.topup" placeholder="请输入内容"></el-input>
+              <el-input v-model="row.topup" placeholder="请输入内容" />
               <el-button
                 type="text"
                 icon="el-icon-check"
+                style="margin-right:8px"
                 @click="
                   row.edit = false;
                   updateTopup(row);
                 "
-                style="margin-right:8px"
               >
                 确认
               </el-button>
-              <el-button type="text" icon="el-icon-close" @click="row.edit = false" style="margin-left:8px">
+              <el-button type="text" icon="el-icon-close" style="margin-left:8px" @click="row.edit = false">
                 取消
               </el-button>
             </template>
@@ -102,8 +105,8 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-import { getDcSummary, updateTopup } from '@/api/performance';
+import { mapGetters } from 'vuex'
+import { getDcSummary, updateTopup } from '@/api/performance'
 
 export default {
   data() {
@@ -112,108 +115,114 @@ export default {
       list: null,
       downloadLoading: false,
       date: new Date().toISOString().slice(0, 10)
-    };
+    }
   },
   created() {
-    this.fetchDcSummary(new Date());
+    this.fetchDcSummary(new Date())
   },
   computed: {
     ...mapGetters(['roles']),
     filename() {
-      return this.date + '-绩效汇总';
+      return this.date + '-绩效汇总'
     }
   },
   methods: {
+    desc() {
+      this.fetchDcSummary(this.date, true);
+    },
+    asc() {
+      this.fetchDcSummary(this.date, false);
+    },
     handleDownload() {
-      this.downloadLoading = true;
+      this.downloadLoading = true
       import('@/vendor/Export2Excel').then(excel => {
-        const tHeader = ['学号', '姓名', '补贴总金额', '第一周DC值', '第一周DC值', '第一周DC值', '第一周DC值', '第一周DC值', '本月总DC值', '当前AC值', 'Topup'];
-        const filterVal = ['stu_num', 'name', 'salary', 'week1', 'week2', 'week3', 'week4', 'week5', 'total', 'ac', 'topup'];
-        const list = this.list;
-        const data = this.formatJson(filterVal, list);
+        const tHeader = ['学号', '姓名', '补贴总金额', '第一周DC值', '第一周DC值', '第一周DC值', '第一周DC值', '第一周DC值', '本月总DC值', '当前AC值', 'Topup']
+        const filterVal = ['stu_num', 'name', 'salary', 'week1', 'week2', 'week3', 'week4', 'week5', 'total', 'ac', 'topup']
+        const list = this.list
+        const data = this.formatJson(filterVal, list)
         excel.export_json_to_excel({
           header: tHeader,
           data,
           filename: this.filename,
           autoWidth: true,
           bookType: 'xlsx'
-        });
-        this.downloadLoading = false;
-        console.log(data);
-      });
+        })
+        this.downloadLoading = false
+        console.log(data)
+      })
     },
     formatJson(filterVal, jsonData) {
       return jsonData.map(v =>
         filterVal.map(j => {
-          console.log(j);
-          return v[j];
+          console.log(j)
+          return v[j]
         })
-      );
+      )
     },
     updateTopup(row) {
-      let vo = {
+      const vo = {
         uid: row.uid,
         yearmonth: this.date.slice(0, 8).replace(/-/g, ''),
         topup: row.topup
-      };
-      console.log(vo);
+      }
+      console.log(vo)
       updateTopup(vo).then(() => {
-        this.date = new Date(this.date);
-        this.date = this.date.toISOString().slice(0, 10);
-        this.fetchDcSummary(this.date);
+        this.date = new Date(this.date)
+        this.date = this.date.toISOString().slice(0, 10)
+        this.fetchDcSummary(this.date)
         this.$message({
           showClose: true,
           message: '编辑成功',
           type: 'success'
-        });
-      });
+        })
+      })
     },
-    fetchDcSummary(date) {
-      getDcSummary(date).then(res => {
+    fetchDcSummary(day, isDesc = true) {
+      getDcSummary({ date: day, desc: isDesc }).then(res => {
         this.list = res.data.map(item => {
-          item.edit = false;
-          return item;
-        });
-        console.log(this.list);
-      });
+          item.edit = false
+          return item
+        })
+        // console.log(this.list)
+      })
     },
     filtrate() {
-      if (this.date != undefined) {
-        this.fetchDcSummary(this.date);
+      if (this.date !== undefined) {
+        this.fetchDcSummary(this.date)
       } else {
         this.$message({
           showClose: true,
           message: '请选择月份',
           type: 'warning'
-        });
+        })
       }
     },
     next() {
-      if (typeof this.date == 'string') {
-        this.date = new Date(this.date);
+      if (typeof this.date === 'string') {
+        this.date = new Date(this.date)
       }
-      this.loading = true;
-      this.date.setMonth(this.date.getMonth() + 1);
-      this.date = this.date.toISOString().slice(0, 10);
-      this.fetchDcSummary(this.date);
+      this.loading = true
+      this.date.setMonth(this.date.getMonth() + 1)
+      this.date = this.date.toISOString().slice(0, 10)
+      this.fetchDcSummary(this.date)
       setTimeout(() => {
-        this.loading = false;
-      }, 400);
+        this.loading = false
+      }, 400)
     },
     prev() {
-      if (typeof this.date == 'string') {
-        this.date = new Date(this.date);
+      if (typeof this.date === 'string') {
+        this.date = new Date(this.date)
       }
-      this.loading = true;
-      this.date.setMonth(this.date.getMonth() - 1);
-      this.date = this.date.toISOString().slice(0, 10);
-      this.fetchDcSummary(this.date);
+      this.loading = true
+      this.date.setMonth(this.date.getMonth() - 1)
+      this.date = this.date.toISOString().slice(0, 10)
+      this.fetchDcSummary(this.date)
       setTimeout(() => {
-        this.loading = false;
-      }, 400);
+        this.loading = false
+      }, 400)
     }
   }
-};
+}
 </script>
 <style scoped>
 .app-container >>> .el-table th.gutter {

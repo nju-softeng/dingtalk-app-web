1b9451f77b072f1269ec218bea604aa1d4958c20
夷 巫 <1343296078@qq.com>
2022-01-24 11:09:25 +0800

    feat: 信息修改

--- src/views/profile/components/personalInfo.vue
+++ src/views/profile/components/personalInfo.vue
@@ -3,51 +3,63 @@
     <div class="personalInfoForm">
       <el-form ref="form" :model="personalInfoForm" label-width="80px">
         <el-row>
-          <el-col :span="4">
-            <el-form-item label="姓名" label-width="55px">
-              <el-input v-model="personalInfoForm.name"/>
+          <el-col :span="8">
+            <el-form-item label="姓名:">
+              <el-input v-model="personalInfoForm.name" />
             </el-form-item>
           </el-col>
-          <el-col :span="5">
-            <el-form-item label="学号" label-width="55px">
+          <el-col :span="8">
+            <el-form-item label="学号:">
               <el-input v-model="personalInfoForm.stuNum" />
             </el-form-item>
           </el-col>
         </el-row>
         <el-row>
-          <el-col :span="7">
-            <el-form-item label="身份证号" >
-              <el-input v-model="personalInfoForm.identityCardId" />
+          <el-col :span="8">
+            <el-form-item label="身份证号:">
+              <el-input v-model="personalInfoForm.idCardNo" />
             </el-form-item>
           </el-col>
-          <el-col :span="7">
-            <el-form-item label="银行卡号" >
-              <el-input v-model="personalInfoForm.bankCardId"/>
+          <el-col :span="8">
+            <el-form-item label="银行卡号:">
+              <el-input v-model="personalInfoForm.creditCard" />
             </el-form-item>
           </el-col>
         </el-row>
         <el-row>
-          <el-col :span="6">
-            <el-form-item label="本科学校" >
-              <el-input v-model="personalInfoForm.undergraduateSchool"/>
+          <el-col :span="8">
+            <el-form-item label="本科学校 :">
+              <el-input v-model="personalInfoForm.undergraduateCollege" />
             </el-form-item>
           </el-col>
-          <el-col :span="6">
-            <el-form-item label="硕士学校" >
-              <el-input v-model="personalInfoForm.masterSchool"/>
+          <el-col :span="8">
+            <el-form-item label="硕士学校:">
+              <el-input v-model="personalInfoForm.masterCollege" />
             </el-form-item>
           </el-col>
-          <el-col :span="5">
-            <el-form-item label="当前状态" >
-              <el-select v-model="personalInfoForm.state" placeholder="请选择">
-                <el-option value="实习">实习</el-option>
-                <el-option value="在校">在校</el-option>
+          <el-col :span="8">
+            <el-form-item label="当前状态:">
+              <el-select v-model="personalInfoForm.workState" placeholder="请选择">
+                <el-option label="实习" :value="true" />
+                <el-option label="在校" :value="false" />
               </el-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="16">
+            <el-form-item label="备注:">
+              <el-input v-model="personalInfoForm.remark" />
             </el-form-item>
           </el-col>
         </el-row>
       </el-form>
     </div>
+    <el-row type="flex" justify="center">
+      <el-col :span="8">
+        <el-button type="primary"  size="large" @click="confirmModify" >确认修改</el-button>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -64,6 +76,11 @@ export default {
     getUserDetail().then(res => {
       this.personalInfoForm = res.data
     })
+  },
+  methods: {
+    confirmModify() {
+      console.log(this.personalInfoForm)
+    }
   }
 }
 </script>
